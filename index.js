d532fe6887b30185e605bbb629c8e6229e921474
Sergey Osipov <limitless7777@yandex.ru>
2023-04-29 21:58:28 +0700

    add music

--- index.js
+++ index.js
@@ -1,126 +1,167 @@
 let phrases = [
   {
-    text: 'Говно'
+    text: 'Говно',
+    music: './music/m1.mp3'
   },
   {
-    text: 'Залупа'
+    text: 'Залупа',
+    music: './music/m2.mp3'
   },
   {
-    text: 'Пенис'
+    text: 'Пенис',
+    music: './music/m3.mp3'
   },
   {
-    text: 'Хер'
+    text: 'Хер',
+    music: './music/m4.mp3'
   },
   {
-    text: 'Давалка'
+    text: 'Давалка',
+    music: './music/m5.mp3'
   },
   {
-    text: 'Хуй'
+    text: 'Хуй',
+    music: './music/m6.mp3'
   },
   {
-    text: 'Блядина'
+    text: 'Блядина',
+    music: './music/m7.mp3'
   },
   {
-    text: 'Головка'
+    text: 'Головка',
+    music: './music/m8.mp3'
   },
   {
-    text: 'Шлюха'
+    text: 'Шлюха',
+    music: './music/m9.mp3'
   },
   {
-    text: 'Жопа'
+    text: 'Жопа',
+    music: './music/m10.mp3'
   },
   {
-    text: 'Член'
+    text: 'Член',
+    music: './music/m11.mp3'
   },
   {
-    text: 'Еблан'
+    text: 'Еблан',
+    music: './music/m12.mp3'
   },
   {
-    text: 'Петух'
+    text: 'Петух',
+    music: './music/m13.mp3'
   },
   {
-    text: 'Мудила'
+    text: 'Мудила',
+    music: './music/m14.mp3'
   },
   {
-    text: 'Рукоблуд'
+    text: 'Рукоблуд',
+    music: './music/m15.mp3'
   },
   {
-    text: 'Ссанина'
+    text: 'Ссанина',
+    music: './music/m16.mp3'
   },
   {
-    text: 'Очко'
+    text: 'Очко',
+    music: './music/m17.mp3'
   },
   {
-    text: 'Блядун'
+    text: 'Блядун',
+    music: './music/m18.mp3'
   },
   {
-    text: 'Вагина'
+    text: 'Вагина',
+    music: './music/m19.mp3'
   },
   {
-    text: 'Сука'
+    text: 'Сука',
+    music: './music/m20.mp3'
   },
   {
-    text: 'Ебланище'
+    text: 'Ебланище',
+    music: './music/m21.mp3'
   },
   {
-    text: 'Влагалище'
+    text: 'Влагалище',
+    music: './music/m22.mp3'
   },
   {
-    text: 'Пердун'
+    text: 'Пердун',
+    music: './music/m23.mp3'
   },
   {
-    text: 'Дрочила'
+    text: 'Дрочила',
+    music: './music/m24.mp3'
   },
   {
-    text: 'Пидор'
+    text: 'Пидор',
+    music: './music/m25.mp3'
   },
   {
-    text: 'Пизда'
+    text: 'Пизда',
+    music: './music/m26.mp3'
   },
   {
-    text: 'Туз'
+    text: 'Туз',
+    music: './music/m27.mp3'
   },
   {
-    text: 'Малафья'
+    text: 'Малафья',
+    music: './music/m28.mp3'
   },
   {
-    text: 'Гомик'
+    text: 'Гомик',
+    music: './music/m29.mp3'
   },
   {
-    text: 'Мудила'
+    text: 'Мудила',
+    music: './music/m30.mp3'
   },
   {
-    text: 'Пилотка'
+    text: 'Пилотка',
+    music: './music/m31.mp3'
   },
   {
-    text: 'Манда'
+    text: 'Манда',
+    music: './music/m32.mp3'
   },
   {
-    text: 'Анус'
+    text: 'Анус',
+    music: './music/m33.mp3'
   },
   {
-    text: 'Вагина'
+    text: 'Вагина',
+    music: './music/m34.mp3'
   },
   {
-    text: 'Путана'
+    text: 'Путана',
+    music: './music/m35.mp3'
   },
   {
-    text: 'Педрила'
+    text: 'Педрила',
+    music: './music/m36.mp3'
   },
   {
-    text: 'Шалава'
+    text: 'Шалава',
+    music: './music/m37.mp3'
   },
   {
-    text: 'Хуила'
+    text: 'Хуила',
+    music: './music/m38.mp3'
   },
   {
-    text: 'Мошонка'
+    text: 'Мошонка',
+    music: './music/m39.mp3'
   },
   {
-    text: 'Елда'
+    text: 'Елда',
+    music: './music/m40.mp3'
   },
   {
-    text: 'РАУНД'
+    text: 'РАУНД',
+    music: './music/m41.mp3'
   }
 ];
 
@@ -133,9 +174,13 @@ let phrase = document.querySelector('.phrase');
 
 let image = document.querySelector('.image');
 
+let music = document.querySelector('.play');
+
 button.addEventListener('click', function () {
   for (; currentIndex < currentLimit && currentIndex < phrases.length; currentIndex++) {
-    smoothly(phrase, 'textContent', phrases[currentIndex].text);
+    phrase.textContent = phrases[currentIndex].text;
+    music.src = phrases[currentIndex].music;
+    music.play();
   }
   if (currentLimit <= phrases.length) {
     currentLimit++;
